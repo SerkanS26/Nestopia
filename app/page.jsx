7e4d7c6231eb16b2fa9ba129d6b31ee76d813ad7
Serkans26 <s.safran84@gmail.com>
2024-05-09 23:57:11 +0200

    pages added

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 className="text-3xl text-green-800">Welcome</h1>
+      <Link href="/properties">Properties</Link>
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ app/properties/page.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const ProperiesPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-softGreen">Properties</h1>
+      <Link href="/">Go Home</Link>
+    </div>
+  );
+};
+
+export default ProperiesPage;
