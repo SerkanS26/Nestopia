414e054652d0087988ca7fb4f4eacb0466a0c097
SerkanS26 <s.safran84@gmail.com>
2024-05-21 17:01:12 +0200

    Property Form & State & Handlers, Post Api and Submit to database

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import PropertAddForm from "@/components/PropertAddForm";
 
 const PropertyAddPage = () => {
-  return <div>PropertyAddPage</div>;
+  return (
+    <section className="bg-softGreen">
+      <div className="container m-auto max-w-2xl py-24">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <PropertAddForm />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PropertyAddPage;
