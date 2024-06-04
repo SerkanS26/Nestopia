028e7d388a9514ccab3e343d99b20aa4a62db362
SerkanS26 <s.safran84@gmail.com>
2024-06-04 19:31:48 +0200

    refactor: Add lazy loading for images on the home page

--- app/page.jsx
+++ app/page.jsx
@@ -3,13 +3,17 @@ import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from "@/components/HomeProperties";
 import FeaturedProperties from "@/components/FeaturedProperties";
 
+import { Suspense } from "react";
+
 const HomePage = async () => {
   return (
     <>
-      <Hero />
-      <InfoBoxes />
-      <FeaturedProperties />
-      <HomeProperties />
+      <Suspense>
+        <Hero />
+        <InfoBoxes />
+        <FeaturedProperties />
+        <HomeProperties />
+      </Suspense>
     </>
   );
 };
