248ce64046a2c66b12350c1ac7f73e697c086fc4
SerkanS26 <s.safran84@gmail.com>
2024-06-05 23:43:18 +0200

    refactor: Enable lazy loading for images in HomePage, InfoBox, and HomeProperties components

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
-// import HomeProperties from "@/components/HomeProperties";
+import HomeProperties from "@/components/HomeProperties";
 import FeaturedProperties from "@/components/FeaturedProperties";
 
 const HomePage = () => {
@@ -9,7 +9,7 @@ const HomePage = () => {
       <Hero />
       <InfoBoxes />
       <FeaturedProperties />
-      {/* <HomeProperties />  */}
+      <HomeProperties />
     </>
   );
 };
