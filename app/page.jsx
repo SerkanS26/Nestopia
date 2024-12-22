c13cc281806e1a77d4c6618275bd1d924313800b
SerkanS26 <s.safran84@gmail.com>
2024-12-22 16:36:37 +0100

    refactor: Uncomment HomeProperties and FeaturedProperties components in HomePage

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,15 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
-// import HomeProperties from "@/components/HomeProperties";
-// import FeaturedProperties from "@/components/FeaturedProperties";
+import HomeProperties from "@/components/HomeProperties";
+import FeaturedProperties from "@/components/FeaturedProperties";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
-      {/* <FeaturedProperties />
-      <HomeProperties /> */}
+      <FeaturedProperties />
+      <HomeProperties />
     </>
   );
 };
